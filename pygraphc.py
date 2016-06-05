44c522e32cbb66332177e0fe2941afd4d0a71e64
Hudan Studiawan <studiawan@gmail.com>
2016-06-05 20:51:14 +0700

    Add action for connected component method and add report (nodes, edges, clusters, and runtime) in the end of program run

--- pygraphc.py
+++ pygraphc.py
@@ -2,7 +2,9 @@ from optparse import OptionParser
 from pygraphc.preprocess.PreprocessLog import PreprocessLog
 from pygraphc.preprocess.CreateGraph import CreateGraph
 from pygraphc.clustering.KCliquePercolation import KCliquePercolation
+from pygraphc.clustering.ConnectedComponents import ConnectedComponents
 from pygraphc.visualization.GraphStreaming import GraphStreaming
+from time import time
 
 
 def main():
@@ -13,17 +15,17 @@ def main():
                       dest='method',
                       choices=['connected_components', 'maxclique_percolation', 'maxclique_percolation_weighted',
                                'kclique_percolation', ],
-                      default='kclique_percolation',
+                      default='connected_components',
                       help='Graph clustering method to run',)
     parser.add_option("-l", "--logfile",
                       action="store",
                       dest="logfile",
-                      default="./data/auth.log.anon.nov.1",
+                      default="./data/auth.log.anon",
                       help="Log file to analyze",)
     parser.add_option("-k", "--kpercolation",
                       action="store",
                       dest="k",
-                      default=3,
+                      default=4,
                       help="Number of k for clique percolation",)
     parser.add_option("-g", "--geometric",
                       action="store",
@@ -33,7 +35,7 @@ def main():
     parser.add_option("-c", "--cosine",
                       action="store",
                       dest="c",
-                      default=0.0,
+                      default=0.2,
                       help="Threshold for cosine similarity while creating graph edges")
 
     (options, args) = parser.parse_args()
@@ -61,13 +63,24 @@ def main():
         kcp.get_kclique_percolation()
         removed_edges = kcp.remove_outcluster()
         clusters = kcp.get_clusters()
+    elif options.method == 'connected_components':
+        cc = ConnectedComponents(graph)
+        clusters = cc.get_clusters()
 
     # graph streaming
     stream = GraphStreaming(graph, edges)
     stream.gephi_streaming()
     stream.change_color(clusters)
-    stream.remove_outcluster(removed_edges)
+    # stream.remove_outcluster(removed_edges)
+
+    print 'Nodes         :', len(nodes_id)
+    print 'Edges         :', len(edges)
+    # print 'Removed edges :', len(removed_edges)
+    print 'Clusters      :', len(clusters)
 
 
 if __name__ == '__main__':
+    start = time()
     main()
+    duration = time() - start
+    print 'Runtime       :', duration, 'seconds'
