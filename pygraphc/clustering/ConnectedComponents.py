066d2db105e4ac1cc5f52c0987c6c6832054bd78
Hudan Studiawan <studiawan@gmail.com>
2016-06-05 20:49:09 +0700

    Rename method get_cluster and revise self.g.node when accessing a node

--- pygraphc/clustering/ConnectedComponents.py
+++ pygraphc/clustering/ConnectedComponents.py
@@ -5,7 +5,7 @@ class ConnectedComponents:
     def __init__(self, g):
         self.g = g
 
-    def get_connected_components(self):
+    def get_clusters(self):
         clusters = []
         for components in nx.connected_components(self.g):
             clusters.append(components)
@@ -13,7 +13,7 @@ class ConnectedComponents:
         cluster_id = 0
         for cluster in clusters:
             for node in cluster:
-                self.g[node]['cluster'] = cluster_id
+                self.g.node[node]['cluster'] = cluster_id
             cluster_id += 1
 
         return clusters
