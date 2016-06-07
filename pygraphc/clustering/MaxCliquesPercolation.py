181d55e3db4977d79f19c95d3b3c596737e61353
Hudan Studiawan <studiawan@gmail.com>
2016-06-07 10:50:08 +0700

    Add self.percolation_dict attribute and change private method using __

--- pygraphc/clustering/MaxCliquesPercolation.py
+++ pygraphc/clustering/MaxCliquesPercolation.py
@@ -28,44 +28,33 @@ class MaxCliquesPercolation:
 class MaxCliquesPercolationWeighted(MaxCliquesPercolation):
     def __init__(self, g, k):
         MaxCliquesPercolation.__init__(self, g, k)
+        self.percolation_dict = {}
 
     def get_maxcliques_percolation_weighted(self):
-        cliques, percolation = self.get_maxcliques_percolation()
-        percolation_dict = self.get_percolation_dict(percolation)
-        for p1, p2 in combinations(percolation, 2):
+        cliques, percolations = self.get_maxcliques_percolation()
+        self.__set_percolation_dict(percolations)
+        for p1, p2 in combinations(percolations, 2):
             intersections = p1.intersection(p2)
             if intersections:
                 for node in intersections:
                     node_neighbors = self.g.neighbors(node)
                     p1_neighbors, p2_neighbors = p1.intersection(node_neighbors), p2.intersection(node_neighbors)
-                    p1_neighbors_weight = self.get_neighbors_weight(node, p1_neighbors)
-                    p2_neighbors_weight = self.get_neighbors_weight(node, p2_neighbors)
+                    p1_neighbors_weight = self.__get_neighbors_weight(node, p1_neighbors)
+                    p2_neighbors_weight = self.__get_neighbors_weight(node, p2_neighbors)
 
-                    percolation_dict[node] = percolation_dict[list(p1_neighbors)[0]] \
-                        if p1_neighbors_weight > p2_neighbors_weight else percolation_dict[list(p2_neighbors)[0]]
+                    self.percolation_dict[node] = self.percolation_dict[list(p1_neighbors)[0]] \
+                        if p1_neighbors_weight > p2_neighbors_weight else self.percolation_dict[list(p2_neighbors)[0]]
 
-        self.set_graph_cluster(percolation_dict)
-        return percolation_dict
-
-    def get_clusters(self, percolation_dict):
-        cluster_ids = set(percolation_dict.values())
-        clusters = []
-        for ids in cluster_ids:
-            cluster = []
-            for node, cluster_id in percolation_dict.iteritems():
-                if ids == cluster_id:
-                    cluster.append(node)
-            clusters.append(cluster)
-
+        self.__set_graph_cluster()
+        clusters = self.__get_clusters()
         return clusters
 
-    def get_percolation_dict(self, percolations):
+    def __set_percolation_dict(self, percolations):
         nodes = self.g.nodes()
-        percolations_dict = {}
         percolations_merged = []
         for index, percolation in enumerate(percolations):
             for p in percolation:
-                percolations_dict[p] = index
+                self.percolation_dict[p] = index
 
             # merge percolations list
             percolations_merged += percolation
@@ -74,19 +63,29 @@ class MaxCliquesPercolationWeighted(MaxCliquesPercolation):
         other_cluster = len(percolations)
         if diff:
             for i in diff:
-                percolations_dict[i] = other_cluster
+                self.percolation_dict[i] = other_cluster
                 other_cluster += 1
 
-        return percolations_dict
-
-    def get_neighbors_weight(self, node, neighbors):
+    def __get_neighbors_weight(self, node, neighbors):
         weight = 0
         for n in neighbors:
             weight += self.g[node][n][0]['weight']
 
         return weight
 
-    def set_graph_cluster(self, percolation_dict):
-        for node in self.g.nodes_iter(data=True):
-            self.g.node[node[0]]['cluster'] = percolation_dict[node[0]]
+    def __set_graph_cluster(self):
+        for node in self.g.nodes_iter(data=True):            
+            self.g.node[node[0]]['cluster'] = self.percolation_dict[node[0]]
         return self.g
+    
+    def __get_clusters(self):
+        cluster_ids = set(self.percolation_dict.values())
+        clusters = []
+        for ids in cluster_ids:
+            cluster = []
+            for node, cluster_id in self.percolation_dict.iteritems():
+                if ids == cluster_id:
+                    cluster.append(node)
+            clusters.append(cluster)
+
+        return clusters
