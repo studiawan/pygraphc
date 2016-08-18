74ae21427e5401f79623a52e47fdd86add517a6c
Hudan Studiawan <studiawan@gmail.com>
2016-08-18 09:00:15 +0700

    Add other evaluation metrics

--- pygraphc/clustering/ClusterEvaluation.py
+++ pygraphc/clustering/ClusterEvaluation.py
@@ -16,3 +16,24 @@ class ClusterEvaluation(object):
         prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
 
         return metrics.adjusted_rand_score(standard_labels, prediction_labels)
+
+    @staticmethod
+    def get_adjusted_mutual_info_score(standard_file, prediction_file):
+        standard_labels = ClusterEvaluation.get_evaluated(standard_file)
+        prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
+
+        return metrics.adjusted_mutual_info_score(standard_labels, prediction_labels)
+
+    @staticmethod
+    def get_normalized_mutual_info_score(standard_file, prediction_file):
+        standard_labels = ClusterEvaluation.get_evaluated(standard_file)
+        prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
+
+        return metrics.normalized_mutual_info_score(standard_labels, prediction_labels)
+
+    @staticmethod
+    def get_homogeneity_completeness_vmeasure(standard_file, prediction_file):
+        standard_labels = ClusterEvaluation.get_evaluated(standard_file)
+        prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
+
+        return metrics.homogeneity_completeness_v_measure(standard_labels, prediction_labels)
