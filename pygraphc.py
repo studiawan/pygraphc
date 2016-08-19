3c61d428937459acd0a1f447e90d8f0c3c94c412
Hudan Studiawan <studiawan@gmail.com>
2016-08-19 09:11:55 +0700

    Add working example for weighted maximal clique percolation

--- pygraphc.py
+++ pygraphc.py
@@ -5,7 +5,9 @@ from pygraphc.clustering.ConnectedComponents import ConnectedComponents
 from pygraphc.clustering.KCliquePercolation import KCliquePercolation, KCliquePercolationWeighted
 from pygraphc.clustering.MaxCliquesPercolation import MaxCliquesPercolation, MaxCliquesPercolationWeighted
 from pygraphc.visualization.GraphStreaming import GraphStreaming
-from time import time
+from time import time, sleep
+from pygraphc.clustering.ClusterUtility import ClusterUtility
+from pygraphc.clustering.ClusterEvaluation import ClusterEvaluation
 
 
 def main():
@@ -16,27 +18,27 @@ def main():
                       dest='method',
                       choices=['connected_components', 'maxclique_percolation', 'maxclique_percolation_weighted',
                                'kclique_percolation', 'kclique_percolation_weighted'],
-                      default='kclique_percolation_weighted',
+                      default='maxclique_percolation_weighted',
                       help='Graph clustering method to run',)
     parser.add_option("-l", "--logfile",
                       action="store",
                       dest="logfile",
-                      default="./data/auth.log.anon",
+                      default="./data/auth.log.anon.16",
                       help="Log file to analyze",)
     parser.add_option("-k", "--kpercolation",
                       action="store",
                       dest="k",
-                      default=3,
+                      default=5,
                       help="Number of k for clique percolation",)
     parser.add_option("-g", "--geometric",
                       action="store",
                       dest="g",
-                      default=0.1,
+                      default=0.3,
                       help="Threshold of geometric mean")
     parser.add_option("-c", "--cosine",
                       action="store",
                       dest="c",
-                      default=0.1,
+                      default=0.0,
                       help="Threshold for cosine similarity while creating graph edges")
 
     (options, args) = parser.parse_args()
@@ -48,6 +50,7 @@ def main():
     p = PreprocessLog(logfile)
     p.do_preprocess()
     events_unique = p.get_eventsunique()
+    logs = p.get_logs()
 
     # create graph
     g = CreateGraph(events_unique, options.c)
@@ -59,12 +62,16 @@ def main():
 
     # k-clique percolation
     clusters, removed_edges = None, None
+    maxcliques = None
+    standard_file = '/home/hudan/Git/labeled-authlog/dataset/161.166.232.16/auth.log.anon.labeled'
+    prediction_file = './results-k=' + str(k) + 'g=' + str(geometric_mean_threshold) + '.log'
     if options.method == 'kclique_percolation':
         kcp = KCliquePercolation(graph, edges_weight, nodes_id, k)
         clusters = kcp.get_kclique_percolation()
     elif options.method == 'kclique_percolation_weighted':
         kcpw = KCliquePercolationWeighted(graph, edges_weight, nodes_id, k, geometric_mean_threshold)
         clusters = kcpw.get_kclique_percolation()
+        removed_edges = kcpw.get_removed_edges()
     elif options.method == 'connected_components':
         cc = ConnectedComponents(graph)
         clusters = cc.get_clusters()
@@ -74,15 +81,29 @@ def main():
     elif options.method == 'maxclique_percolation_weighted':
         mcpw = MaxCliquesPercolationWeighted(graph, edges_weight, nodes_id, k, geometric_mean_threshold)
         clusters = mcpw.get_maxcliques_percolation_weighted()
+        maxcliques = mcpw.get_maxcliques()
+        ClusterUtility.set_cluster_label_id(graph, clusters, logs, prediction_file)
 
     # graph streaming
-    stream = GraphStreaming(graph, edges)
-    stream.gephi_streaming()
-    stream.change_color(clusters)
-    if removed_edges:
-        stream.remove_outcluster(removed_edges)
+    stream_flag = False
+    if stream_flag:
+        stream = GraphStreaming(graph, edges, 0)
+        stream.gephi_streaming()
+        # sleep(120)
+        stream.change_color(maxcliques)
+        if removed_edges:
+            # print 'sleeping for 120 seconds ...'
+            # sleep(120)
+            stream.remove_outcluster(removed_edges)
 
-    return [len(nodes_id), len(edges), len(removed_edges) if removed_edges else 0, len(clusters)]
+    adj_rand_score = ClusterEvaluation.get_adjusted_rand_score(standard_file, prediction_file)
+    adj_mutual_info_score = ClusterEvaluation.get_adjusted_mutual_info_score(standard_file, prediction_file)
+    norm_mutual_info_score = ClusterEvaluation.get_normalized_mutual_info_score(standard_file, prediction_file)
+    homogeneity_completeness_vmeasure = ClusterEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
+                                                                                                prediction_file)
+    return [len(nodes_id), len(edges), len(removed_edges) if removed_edges else 0, len(clusters),
+            options.k, options.g, options.c, adj_rand_score, adj_mutual_info_score, norm_mutual_info_score,
+            homogeneity_completeness_vmeasure]
 
 
 if __name__ == '__main__':
@@ -93,4 +114,11 @@ if __name__ == '__main__':
     print 'Edges         :', properties[1]
     print 'Removed edges :', properties[2]
     print 'Clusters      :', properties[3]
+    print 'k             :', properties[4]
+    print 'I             :', properties[5]
+    print 'C             :', properties[6]
+    print 'Adj rand      :', properties[7]
+    print 'Adj mutual    :', properties[8]
+    print 'Norm mutual   :', properties[9]
+    print 'Hom, comp, v  :', properties[10]
     print 'Runtime       :', duration, 'seconds'
