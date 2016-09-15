925765a498ec98d3c1a280253736ec60f17bbc8d
Hudan Studiawan <studiawan@gmail.com>
2016-09-15 13:30:09 +0700

    Add example for IPLoM

---
+++ pygraphc/misc/IPLoM_example.py
@@ -0,0 +1,11 @@
+from IPLoM import *
+
+RawLogPath = './'
+RawLogFile = 'rawlog.log'
+OutputPath = './results/'
+para=Para(path=RawLogPath, logname=RawLogFile, savePath=OutputPath)
+
+myparser=IPLoM(para)
+time=myparser.mainProcess()
+
+print ('The running time of IPLoM is', time)
