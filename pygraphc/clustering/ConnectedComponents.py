fc52b352cfe30b1d5ab5c05171db26ded31bfb40
Hudan Studiawan <studiawan@gmail.com>
2016-09-19 10:11:36 +0700

    Change to numpydoc style

--- pygraphc/clustering/ConnectedComponents.py
+++ pygraphc/clustering/ConnectedComponents.py
@@ -1,28 +1,31 @@
-"""
-This is a class for connected component detection method to cluster event logs [STU2016]_.
-
-.. [STU2016] H. Studiawan, B. A. Pratomo, and R. Anggoro, Connected component detection for authentication log
-             clustering, in Proceedings of the International Seminar on Science and Technology, 2016, pp. 495-496.
-"""
 import networkx as nx
 
 
 class ConnectedComponents:
-    def __init__(self, g):
-        """
-        This is a constructor for ConnectedComponent class
+    """This is a class for connected component detection method to cluster event logs [1]_.
 
-        :param g: a graph to be clustered
+    .. [1] H. Studiawan, B. A. Pratomo, and R. Anggoro, Connected component detection for authentication log
+       clustering, in Proceedings of the International Seminar on Science and Technology, 2016, pp. 495-496.
+    """
+    def __init__(self, g):
+        """This is a constructor for ConnectedComponent class
+
+        Parameters
+        ----------
+        g : graph
+            a graph to be clustered
         """
         self.g = g
 
     def get_clusters(self):
-        """
-        This method find any connected component in a graph. A component represents a cluster and
-        each component will be given a cluster identifier.
+        """This method find any connected component in a graph.
 
-        :return: Clusters
-        :rtype: list[list]. List of cluster list, where each list contains index (line number) of event log.
+        A component represents a cluster and each component will be given a cluster identifier.
+
+        Returns
+        -------
+        clusters : list[list]
+            List of cluster list, where each list contains index (line number) of event log.
         """
         clusters = []
         for components in nx.connected_components(self.g):
