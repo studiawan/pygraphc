39e166035afb9531ca2d82bfed149822f220703a
Hudan Studiawan <studiawan@gmail.com>
2016-09-27 14:00:15 +0700

    Modify LKE example to display evaluation measurement

--- examples/LKE_example.py
+++ examples/LKE_example.py
@@ -2,13 +2,26 @@
 import sys
 sys.path.insert(0, '../pygraphc/misc')
 from LKE import *
+sys.path.insert(0, '../pygraphc/clustering')
+from ClusterUtility import *
+from ClusterEvaluation import *
 
-RawLogPath = '/home/hudan/Git/labeled-authlog/dataset/161.166.232.17'
-RawLogFile = 'auth.log.anon'
+ip_address = '161.166.232.17'
+standard_path = '/home/hudan/Git/labeled-authlog/dataset/' + ip_address
+standard_file = standard_path + 'auth.log.anon.labeled'
+analyzed_file = 'auth.log.anon'
+prediction_file = 'lke-result-' + ip_address + '.txt'
 OutputPath = './results'
-para = Para(path=RawLogPath, logname=RawLogFile, save_path=OutputPath)
+para = Para(path=standard_path, logname=analyzed_file, save_path=OutputPath)
 
 myparser = LKE(para)
 time = myparser.main_process()
+clusters = myparser.get_clusters()
+original_logs = myparser.get_logs()
 
+ClusterUtility.set_cluster_label_id(None, clusters, original_logs, prediction_file)
+homogeneity_completeness_vmeasure = ClusterEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
+                                                                                            prediction_file)
+
+print homogeneity_completeness_vmeasure
 print ('The running time of LKE is', time)
