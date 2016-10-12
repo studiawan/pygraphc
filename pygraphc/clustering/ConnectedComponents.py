78aabbc9c66bc92fdedec740e32ad9fbd9ee8937
Hudan Studiawan <studiawan@gmail.com>
2016-10-12 17:25:18 +0700

    Change cluster data structure from list to dict

--- pygraphc/clustering/ConnectedComponents.py
+++ pygraphc/clustering/ConnectedComponents.py
@@ -1,4 +1,5 @@
 import networkx as nx
+from ClusterUtility import ClusterUtility
 
 
 class ConnectedComponents:
@@ -7,36 +8,35 @@ class ConnectedComponents:
     References
     ----------
     .. [1] H. Studiawan, B. A. Pratomo, and R. Anggoro, Connected component detection for authentication log
-           clustering, in Proceedings of the International Seminar on Science and Technology, 2016, pp. 495-496.
+           clustering, The 2nd International Seminar on Science and Technology, 2016, pp. 495-496.
     """
-    def __init__(self, g):
-        """This is a constructor for ConnectedComponent class
+    def __init__(self, graph):
+        """This is a constructor for ConnectedComponent class.
 
         Parameters
         ----------
-        g : graph
-            a graph to be clustered
+        graph : graph
+            A graph to be clustered.
         """
-        self.g = g
+        self.graph = graph
 
     def get_clusters(self):
         """This method find any connected component in a graph.
 
         A component represents a cluster and each component will be given a cluster identifier.
+        This method heavily rely on the cosine similarity threshold to build an edge in a graph.
 
         Returns
         -------
-        clusters : list[list]
-            List of cluster list, where each list contains index (line number) of event log.
+        clusters : dict[list]
+            Dictionary of cluster list, where each list contains index (line number) of event log.
         """
-        clusters = []
-        for components in nx.connected_components(self.g):
-            clusters.append(components)
-
+        clusters = {}
         cluster_id = 0
-        for cluster in clusters:
-            for node in cluster:
-                self.g.node[node]['cluster'] = cluster_id
+        for components in nx.connected_components(self.graph):
+            clusters[cluster_id] = components
             cluster_id += 1
+
+        ClusterUtility.set_cluster_id(self.graph, clusters)
 
         return clusters
