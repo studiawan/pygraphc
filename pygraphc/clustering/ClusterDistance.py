79ce010eeb7db74933ccc7dcdf56a0b5262cd906
Hudan Studiawan <studiawan@gmail.com>
2016-10-16 22:19:26 +0700

    Add dosctring to ClusterDistance

--- pygraphc/clustering/ClusterDistance.py
+++ pygraphc/clustering/ClusterDistance.py
@@ -1,7 +1,30 @@
 
 class ClusterDistance(object):
+    """Class for distance measurement between two vertices.
+    """
     @staticmethod
     def get_cosine_similarity(tfidf1, tfidf2, length1, length2):
+        """Measure cosine similarity between two strings.
+
+        Typically, it is similarity between two event log messages in the two vertices in
+        graph-based event log clustering.
+
+        Parameters
+        ----------
+        tfidf1  : list
+            List of tf-idf measure from the first message.
+        tfidf2  : list
+            List of tf-idf measure from the second message.
+        length1 : float
+            Denominator in cosine similarity from the first message.
+        length2 : float
+            Denominator in cosine similarity from the second message.
+
+        Returns
+        -------
+        cosine_similarity   : float
+            Cosine similarity between two strings.
+        """
         vector_products = 0
         for ti1 in tfidf1:
             for ti2 in tfidf2:
@@ -13,4 +36,5 @@ class ClusterDistance(object):
         except ZeroDivisionError:
             cosine_similarity = 0
 
-        return round(cosine_similarity, 3)
+        cosine_similarity = round(cosine_similarity, 3)
+        return cosine_similarity
