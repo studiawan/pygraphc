19b57171e22eeeba4765f8d822d468b520bd79be
Hudan Studiawan <studiawan@gmail.com>
2016-10-21 13:37:27 +0700

    Change stream_flag to options

--- pygraphc.py
+++ pygraphc.py
@@ -4,11 +4,12 @@ from pygraphc.preprocess.CreateGraph import CreateGraph
 from pygraphc.clustering.ConnectedComponents import ConnectedComponents
 from pygraphc.clustering.KCliquePercolation import KCliquePercolation, KCliquePercolationWeighted
 from pygraphc.clustering.MaxCliquesPercolation import MaxCliquesPercolation, MaxCliquesPercolationWeighted
-from pygraphc.clustering.MajorClust import MajorClust
+from pygraphc.clustering.MajorClust import MajorClust, ImprovedMajorClust
 from pygraphc.visualization.GraphStreaming import GraphStreaming
 from time import time  # , sleep
 from pygraphc.clustering.ClusterUtility import ClusterUtility
 from pygraphc.clustering.ClusterEvaluation import ClusterEvaluation
+from pygraphc.anomaly.AnomalyScore import AnomalyScore
 
 
 def main():
@@ -35,15 +36,15 @@ def main():
     adj_mutual_info_score               : float
         Adjusted mutual information.
     norm_mutual_info_score              : float
-         Normalized mutual information.
+        Normalized mutual information.
     homogeneity_completeness_vmeasure   : float
         Homogeneity, completeness and V-measure.
     """
-    standard_path = '/home/hudan/Git/labeled-authlog/dataset/Hofstede2014/dataset1/161.166.232.17/'
-    standard_file = standard_path + 'auth.log.anon.labeled'
-    analyzed_file = standard_path + 'auth.log.anon'
+    groundtruth_path = '/home/hudan/Git/labeled-authlog/dataset/Hofstede2014/dataset1/161.166.232.17/'
+    groundtruth_file = groundtruth_path + 'auth.log.anon.labeled'
+    analyzed_file = groundtruth_path + 'auth.log.anon'
     graph_method = ['connected_components', 'maxclique_percolation', 'maxclique_percolation_weighted',
-                    'kclique_percolation', 'kclique_percolation_weighted', 'majorclust']
+                    'kclique_percolation', 'kclique_percolation_weighted', 'majorclust', 'improved_majorclust']
     nongraph_method = ['iplom', 'lke']
     methods = graph_method + nongraph_method
 
@@ -53,28 +54,33 @@ def main():
                       action='store',
                       dest='method',
                       choices=methods,
-                      default='maxclique_percolation_weighted',
-                      help='Graph clustering method to run',)
-    parser.add_option("-l", "--logfile",
-                      action="store",
-                      dest="logfile",
+                      default='majorclust',
+                      help='Graph clustering method to run.')
+    parser.add_option('-l', '--logfile',
+                      action='store',
+                      dest='logfile',
                       default=analyzed_file,
-                      help="Log file to analyze",)
-    parser.add_option("-k", "--kpercolation",
-                      action="store",
-                      dest="k",
+                      help='Log file to analyze.')
+    parser.add_option('-k', '--kpercolation',
+                      action='store',
+                      dest='k',
                       default=3,
-                      help="Number of k for clique percolation",)
-    parser.add_option("-g", "--geometric",
-                      action="store",
-                      dest="g",
+                      help='Number of k for clique percolation.')
+    parser.add_option('-g', '--geometric',
+                      action='store',
+                      dest='g',
                       default=0.2,
-                      help="Threshold of geometric mean")
-    parser.add_option("-c", "--cosine",
-                      action="store",
-                      dest="c",
+                      help='Threshold of geometric mean.')
+    parser.add_option('-c', '--cosine',
+                      action='store',
+                      dest='c',
                       default=0.0,
-                      help="Threshold for cosine similarity while creating edges")
+                      help='Threshold for cosine similarity while creating edges.')
+    parser.add_option('-s', '--stream',
+                      action='store',
+                      dest='s',
+                      default=False,
+                      help='Streaming the processed graph to Gephi.')
 
     # get options
     (options, args) = parser.parse_args()
@@ -102,9 +108,7 @@ def main():
         edges = g.get_edges_dict()
         edges_weight = g.get_edges_weight()
         nodes_id = g.get_nodes_id()
-
         # maxcliques = None
-        stream_flag = False
 
         # prediction result file
         if options.method in ['maxclique_percolation', 'maxclique_percolation_weighted', 'kclique_percolation',
@@ -113,55 +117,66 @@ def main():
                               '-c=' + str(options.c) + '.log'
         elif options.method in ['connected_components']:
             prediction_file = './results-c=' + str(options.c) + '.log'
-        elif options.method in ['majorclust']:
+        elif options.method in ['majorclust', 'improved_majorclust']:
             prediction_file = './results.log'
 
         # run the selected method
         if options.method == 'kclique_percolation':
             kcp = KCliquePercolation(graph, edges_weight, nodes_id, k)
             clusters = kcp.get_kclique_percolation()
-            if stream_flag:
+            if options.s:
                 graph_streaming(graph, edges, removed_edges)
         elif options.method == 'kclique_percolation_weighted':
             kcpw = KCliquePercolationWeighted(graph, edges_weight, nodes_id, k, geometric_mean_threshold)
             clusters = kcpw.get_kclique_percolation()
             removed_edges = kcpw.get_removed_edges()
-            if stream_flag:
+            if options.s:
                 graph_streaming(graph, edges, removed_edges)
         elif options.method == 'connected_components':
             cc = ConnectedComponents(graph)
             clusters = cc.get_clusters()
-            if stream_flag:
+            if options.s:
                 graph_streaming(graph, edges, None)
         elif options.method == 'maxclique_percolation':
             mcp = MaxCliquesPercolation(graph, edges_weight, nodes_id, k)
             clusters = mcp.get_maxcliques_percolation()
-            if stream_flag:
+            if options.s:
                 graph_streaming(graph, edges, removed_edges)
         elif options.method == 'maxclique_percolation_weighted':
             mcpw = MaxCliquesPercolationWeighted(graph, edges_weight, nodes_id, k, geometric_mean_threshold)
             clusters = mcpw.get_maxcliques_percolation_weighted()
             # maxcliques = mcpw.get_maxcliques()
-            if stream_flag:
+            if options.s:
                 graph_streaming(graph, edges, removed_edges)
         elif options.method == 'majorclust':
-            # majorclust still does not work, need further debugging
+            # please note that this method is not suitable for a dense graph.
             mc = MajorClust(graph)
             clusters = mc.get_majorclust()
-            if stream_flag:
+            print clusters
+            if options.s:
+                graph_streaming(graph, edges, removed_edges)
+        elif options.method == 'improved_majorclust':
+            imc = ImprovedMajorClust(graph)
+            clusters = imc.get_improved_majorclust()
+            print clusters
+            if options.s:
                 graph_streaming(graph, edges, removed_edges)
 
         # get prediction file
         ClusterUtility.set_cluster_label_id(graph, clusters, logs, prediction_file)
 
+        # get anomaly score
+        anomaly_score = AnomalyScore(graph, clusters, '')
+        anomaly_score.write_property()
+
     elif options.method in nongraph_method:
         pass
 
     # get evaluation of clustering performance
-    adj_rand_score = ClusterEvaluation.get_adjusted_rand_score(standard_file, prediction_file)
-    adj_mutual_info_score = ClusterEvaluation.get_adjusted_mutual_info_score(standard_file, prediction_file)
-    norm_mutual_info_score = ClusterEvaluation.get_normalized_mutual_info_score(standard_file, prediction_file)
-    homogeneity_completeness_vmeasure = ClusterEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
+    adj_rand_score = ClusterEvaluation.get_adjusted_rand_score(groundtruth_file, prediction_file)
+    adj_mutual_info_score = ClusterEvaluation.get_adjusted_mutual_info_score(groundtruth_file, prediction_file)
+    norm_mutual_info_score = ClusterEvaluation.get_normalized_mutual_info_score(groundtruth_file, prediction_file)
+    homogeneity_completeness_vmeasure = ClusterEvaluation.get_homogeneity_completeness_vmeasure(groundtruth_file,
                                                                                                 prediction_file)
     return [len(nodes_id), len(edges), len(removed_edges) if removed_edges else 0, len(clusters),
             options.k, options.g, options.c, adj_rand_score, adj_mutual_info_score, norm_mutual_info_score,
