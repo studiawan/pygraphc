247475ced117f3f38d6805d0edea2569882eb006
Hudan Studiawan <studiawan@gmail.com>
2016-10-26 14:41:55 +0700

    Add docstring for CreateGraph

--- pygraphc/preprocess/CreateGraph.py
+++ pygraphc/preprocess/CreateGraph.py
@@ -3,8 +3,19 @@ from itertools import combinations
 from pygraphc.clustering.ClusterDistance import ClusterDistance
 
 
-class CreateGraph:
+class CreateGraph(object):
+    """A class for generating graph from preprocessed logs.
+    """
     def __init__(self, events_unique, cosine_threshold):
+        """Constructor for class CreateGraph.
+
+        Parameters
+        ----------
+        events_unique       : list[str]
+            List of unique events from preprocessed logs.
+        cosine_threshold    : float
+            Threshold of cosine similarity measure for edge weight.
+        """
         self.events_unique = events_unique
         self.g = nx.MultiGraph()
         self.edges_dict = {}
@@ -18,6 +29,8 @@ class CreateGraph:
         return self.edges_weight
 
     def do_create(self):
+        """Main method to be executed to create a graph.
+        """
         self.__create_nodes()
         self.__create_edges()
 
@@ -25,14 +38,25 @@ class CreateGraph:
         return self.g
 
     def get_nodes_id(self):
+        """Get node identifier.
+
+        Returns
+        -------
+        nodes   : list[int]
+            List of node identifier in incremental integer.
+        """
         total_nodes = len(self.events_unique)
         nodes = [i for i in xrange(total_nodes)]
         return nodes
 
     def __create_nodes(self):
+        """Create nodes in the graph from unique events.
+        """
         self.g.add_nodes_from(self.events_unique)
 
     def __create_edges(self):
+        """Create all edges in the graph based on cosine similarity measure.
+        """
         edges_combinations = [eu[0] for eu in self.events_unique]
         edge_index = 0
         for ec in combinations(edges_combinations, 2):
@@ -41,7 +65,7 @@ class CreateGraph:
             length1, length2 = self.g.node[ec[0]]['length'], self.g.node[ec[1]]['length']
             cosine_similarity = ClusterDistance.get_cosine_similarity(tfidf1, tfidf2, length1, length2)
 
-            # create edge
+            # create edge if cosine similarity measure is bigger then threshold
             if cosine_similarity > self.cosine_threshold:
                 self.g.add_edge(ec[0], ec[1], weight=cosine_similarity)
                 self.edges_weight.append((ec[0], ec[1], cosine_similarity))
