f24a4ea6311e116537396717a534ca89946ab831
Hudan Studiawan <studiawan@gmail.com>
2016-10-29 09:58:52 +0700

    Add separate methods for homogeneity, completeness, and V-measure

--- pygraphc/clustering/ClusterEvaluation.py
+++ pygraphc/clustering/ClusterEvaluation.py
@@ -121,4 +121,64 @@ class ClusterEvaluation(object):
 
         return homogeneity_completeness_vmeasure
 
+    @staticmethod
+    def get_homogeneity(standard_file, prediction_file):
+        """Get homogeneity score.
 
+        Parameters
+        ----------
+        standard_file   : str
+            The ground truth or standard filename.
+        prediction_file : str
+            The analyzed or predicted filename.
+
+        Returns
+        -------
+        homogeneity_score   : float
+            Homogeneity score.
+        """
+        standard_labels = ClusterEvaluation.get_evaluated(standard_file)
+        prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
+        homogeneity_score = metrics.homogeneity_score(standard_labels, prediction_labels)
+
+        return homogeneity_score
+
+    @staticmethod
+    def get_completeness(standard_file, prediction_file):
+        """Get completeness score.
+
+        Parameters
+        ----------
+        standard_file   : str
+        prediction_file : str
+
+        Returns
+        -------
+        completeness_score  : float
+            Completeness score.
+        """
+        standard_labels = ClusterEvaluation.get_evaluated(standard_file)
+        prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
+        completeness_score = metrics.completeness_score(standard_labels, prediction_labels)
+
+        return completeness_score
+
+    @staticmethod
+    def get_vmeasure(standard_file, prediction_file):
+        """Get V-measure score.
+
+        Parameters
+        ----------
+        standard_file   : str
+        prediction_file : str
+
+        Returns
+        -------
+        vmeasure_score  : float
+            V-measure score.
+        """
+        standard_labels = ClusterEvaluation.get_evaluated(standard_file)
+        prediction_labels = ClusterEvaluation.get_evaluated(prediction_file)
+        vmeasure_score = metrics.v_measure_score(standard_labels, prediction_labels)
+
+        return vmeasure_score
