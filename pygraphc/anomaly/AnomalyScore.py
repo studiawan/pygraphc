2ba90a737e6679e601b15c2d82274cf66f05c462
Hudan Studiawan <studiawan@gmail.com>
2016-10-29 07:23:33 +0700

    Add docstring to AnomalyScore

--- pygraphc/anomaly/AnomalyScore.py
+++ pygraphc/anomaly/AnomalyScore.py
@@ -4,7 +4,17 @@ import csv
 
 
 class AnomalyScore(object):
+    """A class to calculate anomaly score in a cluster.
+    """
     def __init__(self, graph, clusters, filename):
+        """The constructor of class AnomalyScore.
+
+        Parameters
+        ----------
+        graph       : graph
+        clusters    : dict[list]
+        filename    : str
+        """
         self.graph = graph
         self.clusters = clusters
         self.filename = filename
@@ -12,6 +22,8 @@ class AnomalyScore(object):
         self.abstraction = {}
 
     def write_property(self):
+        """Write cluster property to a file.
+        """
         # get cluster abstraction and its properties
         self.abstraction = ClusterAbstraction.dp_lcs(self.graph, self.clusters)
         self.property = ClusterUtility.get_cluster_property(self.graph, self.clusters)
