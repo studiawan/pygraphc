765102da6f1bf40b3df34d3b3af1d08158fbbdbc
Hudan Studiawan <studiawan@gmail.com>
2016-10-31 11:41:36 +0700

    Add docstring to GraphStreaming

--- pygraphc/visualization/GraphStreaming.py
+++ pygraphc/visualization/GraphStreaming.py
@@ -4,7 +4,20 @@ from random import uniform
 
 
 class GraphStreaming(object):
+    """A class to stream the graph to Gephi.
+    """
     def __init__(self, graph_clusters, edges, sleep_time=0):
+        """The constructor of class GraphStreaming.
+
+        Parameters
+        ----------
+        graph_clusters  : graph
+            A graph to be visualized in Gephi.
+        edges           : dict
+            Dictionary of edges. Key: (node1, node2). Value: edge id
+        sleep_time      : int
+            Time for delaying display in Gephi.
+        """
         self.g = graph_clusters
         self.edges = edges
         self.sleep_time = sleep_time
@@ -12,6 +25,13 @@ class GraphStreaming(object):
         self.gstream.clean()
 
     def change_color(self, colored_object):
+        """Change color for a particular object, such as a clique or a cluster.
+
+        Parameters
+        ----------
+        colored_object  : list
+            List of object to be colored.
+        """
         # change node color based on objects, e.g., cluster, k-clique, or valid k-cliques
         object_color = self.__set_node_color(colored_object, [])
         for index, objects in enumerate(colored_object):
@@ -28,6 +48,8 @@ class GraphStreaming(object):
                 self.gstream.change_node(node, **node_attributes)
 
     def gephi_streaming(self):
+        """Stream the graph to Gephi.
+        """
         # streaming nodes
         print 'streaming node ...'
         for node in self.g.nodes_iter(data=True):
@@ -50,6 +72,13 @@ class GraphStreaming(object):
             self.gstream.add_edge(edge_index, e[0], e[1], weight=weight[0]['weight'], directed=False)
 
     def remove_outcluster(self, removed_edges):
+        """Remove edge connecting two nodes in different clusters.
+
+        Parameters
+        ----------
+        removed_edges   : list[tuple]
+            List of tuple containing two connecting nodes.
+        """
         # remove edge outside cluster
         for removed_edge in removed_edges:
             try:
@@ -57,7 +86,22 @@ class GraphStreaming(object):
             except KeyError:
                 self.gstream.delete_edge(self.edges[(removed_edge[1], removed_edge[0])])
 
-    def __set_node_color(self, colored_object, single_color):
+    @staticmethod
+    def __set_node_color(colored_object, single_color):
+        """Set unique node color per cluster.
+
+        Parameters
+        ----------
+        colored_object  : iterable
+            List of object to be colored.
+        single_color    : list
+            Flag for single color.
+
+        Returns
+        -------
+        object_color    : list[tuple]
+            List color for each object.
+        """
         object_length = len(colored_object)
         object_color = [single_color for _ in range(object_length)] if single_color else \
             [[uniform(0.0, 1.0) for _ in range(3)] for _ in range(object_length)]
