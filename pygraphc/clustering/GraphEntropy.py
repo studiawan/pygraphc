02b290857a00b4429dc94843677643ff69b51c15
Hudan Studiawan <studiawan@gmail.com>
2016-11-03 11:42:23 +0700

    Add docstring for GraphEntropy

--- pygraphc/clustering/GraphEntropy.py
+++ pygraphc/clustering/GraphEntropy.py
@@ -3,10 +3,38 @@ from ClusterUtility import ClusterUtility
 
 
 class GraphEntropy(object):
+    """A class for graph clustering using entropy-based method [Kenley2011]_. The review of
+       some graph clustering method for PPI networks, including this entropy-based, was presented in [Price2013]_.
+       This code is NetworkX version of the original code by True Price [Price2016]_.
+
+    References
+    ----------
+    .. [Kenley2011] Edward Casey Kenley and Young-Rae Cho, Detecting protein complexes and functional modules
+                    from protein interaction networks: A graph entropy approach,
+                    Proteomics 2011, 11, pp. 3835-3844.
+    .. [Price2013]  True Price, Francisco I Pena III, Young-Rae Cho, Survey: Enhancing Protein Complex Prediction in
+                    PPI Networks with GO Similarity Weighting, Interdisciplinary Sciences: Computational Life Sciences,
+                    5(3), pp. 196-210, 2013.
+    .. [Price2016]  True Price, Graph clustering in Python, https://github.com/trueprice/python-graph-clustering.
+    """
     def __init__(self, graph):
+        """The constructor for class GraphEntropy.
+
+        Parameters
+        ----------
+        graph   : graph
+            A graph to be clustered.
+        """
         self.graph = graph
 
     def get_graph_entropy(self):
+        """The main method to execute clustering based on graph entropy.
+
+        Returns
+        -------
+        clusters    : dict[list]
+            Dictionary of list containing nodes for each cluster.
+        """
         nodes = set(self.graph.nodes())
         clusters = {}
         cluster_id = 0
@@ -56,7 +84,20 @@ class GraphEntropy(object):
         return clusters
 
     def _get_entropies(self, cluster_candidate, neighbors):
-        # get entropy from cluster candidate or all nodes in a graph.
+        """Get entropy from cluster candidate or all nodes in a graph.
+
+        Parameters
+        ----------
+        cluster_candidate   : iterable
+            The entity to be analyzed for its entropy. It can be cluster candidate or a graph.
+        neighbors           : list
+            The neigbors of processed node.
+
+        Returns
+        -------
+        entropies   : dict
+            Dictionary of entropies. Key: node identifier and value: entropy.
+        """
         entropies = {}
         for node in neighbors:
             entropies[node] = self._get_node_entropy(cluster_candidate, node)
@@ -64,6 +105,20 @@ class GraphEntropy(object):
         return entropies
 
     def _get_node_entropy(self, cluster_candidate, node):
+        """Get node's entropy.
+
+        Parameters
+        ----------
+        cluster_candidate   : set
+            The entity to be analyzed for its entropy. It can be cluster candidate or a graph.
+        node                : int
+            The analyzed node to be calculated for its entropy.
+
+        Returns
+        -------
+        entropy : float
+            Entropy for a particular node.
+        """
         # get node degree with weight
         degree = self.graph.degree(weight='weight')[node]
         if degree == 0:
