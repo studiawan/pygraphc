5619b04f55418c943e7a7af606b5247ce4441160
Hudan Studiawan <studiawan@gmail.com>
2016-11-05 08:39:37 +0700

    Change module path for cluster evaluation

--- examples/IPLoM_example.py
+++ examples/IPLoM_example.py
@@ -2,9 +2,8 @@
 import sys
 sys.path.insert(0, '../pygraphc/misc')
 from IPLoM import *
-sys.path.insert(0, '../pygraphc/clustering')
-from ClusterUtility import *
-from ClusterEvaluation import *
+sys.path.insert(0, '../pygraphc/evaluation')
+from ExternalEvaluation import *
 
 # set path
 ip_address = '161.166.232.17'
@@ -19,11 +18,11 @@ para = Para(path=standard_path, logname=analyzed_file, save_path=OutputPath)
 myparser = IPLoM(para)
 time = myparser.main_process()
 clusters = myparser.get_clusters()
-original_logs = myparser.get_logs()
+original_logs = myparser.logs
 
 # set cluster label to get evaluation metrics
-ClusterUtility.set_cluster_label_id(None, clusters, original_logs, prediction_file)
-homogeneity_completeness_vmeasure = ClusterEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
+ExternalEvaluation.set_cluster_label_id(None, clusters, original_logs, prediction_file)
+homogeneity_completeness_vmeasure = ExternalEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
                                                                                             prediction_file)
 
 print homogeneity_completeness_vmeasure
