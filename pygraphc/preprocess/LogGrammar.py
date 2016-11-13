4d5481fd48fbf4047c360fdd62dfa5b0d2eb49be
Hudan Studiawan <studiawan@gmail.com>
2016-11-14 05:58:16 +0700

    Add docstring to LogGrammar

--- pygraphc/preprocess/LogGrammar.py
+++ pygraphc/preprocess/LogGrammar.py
@@ -2,11 +2,22 @@ from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, R
 
 
 class LogGrammar(object):
+    """A class to define the format (grammar) of a log file. We heavily rely on pyparsing in this case.
+    """
     def __init__(self, log_line):
-        self.log_line = log_line
-        self.authlog_grammar = None
+        """The constructor of LogGrammar.
 
-    def get_authlog_grammar(self):
+        Parameters
+        ----------
+        log_line    : str
+            A log line to be parsed.
+        """
+        self.log_line = log_line
+
+    @staticmethod
+    def __get_authlog_grammar():
+        """The definition of auth.log grammar.
+        """
         ints = Word(nums)
 
         # timestamp
@@ -21,12 +32,23 @@ class LogGrammar(object):
         message = Regex(".*")
 
         # auth log grammar
-        self.authlog_grammar = timestamp + hostname + appname + message
+        authlog_grammar = timestamp + hostname + appname + message
+        return authlog_grammar
 
     def parse_authlog(self):
-        parsed = {}
-        parsed_authlog = self.authlog_grammar.parseString(self.log_line)
+        """Parse auth.log based on defined grammar.
 
+        Returns
+        -------
+        parsed  : dict[str, str]
+            A parsed auth.log containing these elements: timestamp, hostname, service, pid, and message.
+        """
+        # get auth.log grammar
+        authlog_grammar = self.__get_authlog_grammar()
+        parsed_authlog = authlog_grammar.parseString(self.log_line)
+
+        # get parsed auth.log
+        parsed = dict()
         parsed['timestamp'] = parsed_authlog[0] + ' ' + parsed_authlog[1] + ' ' + parsed_authlog[2]
         parsed['hostname'] = parsed_authlog[3]
         parsed['service'] = parsed_authlog[4]
