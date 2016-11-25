3a339312a1b9e27fe7d9798f4a7eb78032be0fd0
Hudan Studiawan <studiawan@gmail.com>
2016-11-25 15:06:58 +0700

    Edit import module, input path, and add some comments. pygraphc is now ready to be packaged

--- examples/LKE_example.py
+++ examples/LKE_example.py
@@ -1,26 +1,25 @@
-# for local run, before pygraphc packaging
-import sys
-sys.path.insert(0, '../pygraphc/misc')
-from LKE import *
-sys.path.insert(0, '../pygraphc/evaluation')
-from ExternalEvaluation import *
+from pygraphc.misc.LKE import *
+from pygraphc.evaluation.ExternalEvaluation import *
 
+# set input and output path
 ip_address = '161.166.232.17'
-standard_path = '/home/hudan/Git/labeled-authlog/dataset/' + ip_address
+standard_path = '/home/hudan/Git/labeled-authlog/dataset/Hofstede2014/dataset1/' + ip_address
 standard_file = standard_path + 'auth.log.anon.labeled'
 analyzed_file = 'auth.log.anon'
 prediction_file = 'lke-result-' + ip_address + '.txt'
 OutputPath = './results'
 para = Para(path=standard_path, logname=analyzed_file, save_path=OutputPath)
 
+# run LKE method
 myparser = LKE(para)
 time = myparser.main_process()
 clusters = myparser.get_clusters()
 original_logs = myparser.logs
 
+# perform evaluation
 ExternalEvaluation.set_cluster_label_id(None, clusters, original_logs, prediction_file)
 homogeneity_completeness_vmeasure = ExternalEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
-                                                                                            prediction_file)
-
+                                                                                             prediction_file)
+# print evaluation result
 print homogeneity_completeness_vmeasure
 print ('The running time of LKE is', time)
