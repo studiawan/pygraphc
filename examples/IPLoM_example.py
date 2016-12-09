1c43affbd82f68ed8956cd407c494ff46dab9203
Hudan Studiawan <studiawan@gmail.com>
2016-12-09 16:59:50 +0700

    Edit path and external evaluation

--- examples/IPLoM_example.py
+++ examples/IPLoM_example.py
@@ -2,13 +2,12 @@ from pygraphc.misc.IPLoM import *
 from pygraphc.evaluation.ExternalEvaluation import *
 
 # set input path
-ip_address = '161.166.232.17'
-standard_path = '/home/hudan/Git/labeled-authlog/dataset/Hofstede2014/dataset1/' + ip_address
-standard_file = standard_path + 'auth.log.anon.labeled'
-analyzed_file = 'auth.log.anon'
-prediction_file = 'iplom-result-' + ip_address + '.txt'
-OutputPath = './result'
-para = Para(path=standard_path, logname=analyzed_file, save_path=OutputPath)
+dataset_path = '/home/hudan/Git/labeled-authlog/dataset/Hofstede2014/dataset1_perday/'
+groundtruth_file = dataset_path + 'Dec 1.log.labeled'
+analyzed_file = 'Dec 1.log'
+OutputPath = '/home/hudan/Git/pygraphc/result/misc/'
+prediction_file = OutputPath + 'Dec 1.log.perline'
+para = Para(path=dataset_path, logname=analyzed_file, save_path=OutputPath)
 
 # call IPLoM and get clusters
 myparser = IPLoM(para)
@@ -18,8 +17,15 @@ original_logs = myparser.logs
 
 # set cluster label to get evaluation metrics
 ExternalEvaluation.set_cluster_label_id(None, clusters, original_logs, prediction_file)
-homogeneity_completeness_vmeasure = ExternalEvaluation.get_homogeneity_completeness_vmeasure(standard_file,
-                                                                                             prediction_file)
+
+# get evaluation of clustering performance
+ar = ExternalEvaluation.get_adjusted_rand(groundtruth_file, prediction_file)
+ami = ExternalEvaluation.get_adjusted_mutual_info(groundtruth_file, prediction_file)
+nmi = ExternalEvaluation.get_normalized_mutual_info(groundtruth_file, prediction_file)
+h = ExternalEvaluation.get_homogeneity(groundtruth_file, prediction_file)
+c = ExternalEvaluation.get_completeness(groundtruth_file, prediction_file)
+v = ExternalEvaluation.get_vmeasure(groundtruth_file, prediction_file)
+
 # print evaluation result
-print homogeneity_completeness_vmeasure
+print ar, ami, nmi, h, c, v
 print ('The running time of IPLoM is', time)
