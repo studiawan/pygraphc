81d41ceaf89848851d8353021f01f41c724aaae8
Hudan Studiawan <studiawan@gmail.com>
2016-12-16 09:23:12 +0700

    Add docstring to PySplunk

--- pygraphc/misc/splunk/pysplunk.py
+++ pygraphc/misc/splunk/pysplunk.py
@@ -2,7 +2,29 @@ from os import system, remove
 
 
 class PySplunk(object):
+    """Get log clustering using Python Splunk API [SplunkDev2016]_.
+
+    References
+    ----------
+    .. [SplunkDev2016] Command line examples in the Splunk SDK for Python.
+                       http://dev.splunk.com/view/python-sdk/SP-CAAAEFK
+    """
     def __init__(self, username, source, host, output_mode, tmp_file='/tmp/pysplunk_cluster.csv'):
+        """The constructor of class PySplunk.
+
+        Parameters
+        ----------
+        username        : str
+            Username to access Splunk daemon. No password required since we use Splunk free version.
+        source          : str
+            Identifier for log source. It is usually filename of log.
+        host            : str
+            Hostname for the source log.
+        output_mode     : str
+            Output for clustering result. Recommended output is csv
+        tmp_file        : str
+            Path for temporary clustering result.
+        """
         self.username = username
         self.source = source.replace(' ', '\ ')
         self.host = host
@@ -10,6 +32,13 @@ class PySplunk(object):
         self.tmp_file = tmp_file
 
     def get_splunk_cluster(self):
+        """Get log clusters.
+
+        Returns
+        -------
+        clusters    : dict
+            Dictionary of log cluster. Key: cluster_id, value: list of log line identifier.
+        """
         # run Python Splunk API command
         command = 'python search.py --username=' + self.username + ' "search source=' + self.source + \
                   ' host=' + self.host + ' sourcetype=linux_secure | cluster labelfield=cluster_id labelonly=t |' \
@@ -28,5 +57,4 @@ class PySplunk(object):
 
         # remove tmp_file
         remove(self.tmp_file)
-
         return clusters
