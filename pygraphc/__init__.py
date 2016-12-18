58bfb5266eb7624f4a8ab5a64d44bfb83599b2f6
Hudan Studiawan <studiawan@gmail.com>
2016-12-18 10:49:35 +0700

    Remove scripts in setup.py

---
+++ pygraphc/__init__.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+import pygraphc.abstraction
+from pygraphc.abstraction import *
+
+import pygraphc.anomaly
+from pygraphc.anomaly import *
+
+import pygraphc.clustering
+from pygraphc.clustering import *
+
+import pygraphc.evaluation
+from pygraphc.evaluation import *
+
+import pygraphc.misc
+from pygraphc.misc import *
+
+import pygraphc.output
+from pygraphc.output import *
+
+import pygraphc.preprocess
+from pygraphc.preprocess import *
+
+import pygraphc.pygephi
+from pygraphc.pygephi import *
+
+import pygraphc.visualization
+from pygraphc.visualization import *
