557fddcf26ef52ccea3761b000e5a94e3f551a78
Hudan Studiawan <studiawan@gmail.com>
2017-03-26 12:50:17 +0800

    Edit the parameters. Not fix yet

--- pygraphc/optimization/SimulatedAnnealing.py
+++ pygraphc/optimization/SimulatedAnnealing.py
@@ -3,40 +3,50 @@ from pygraphc.evaluation.InternalEvaluation import InternalEvaluation
 
 
 class SimulatedAnnealing(object):
-    def __init__(self, method, tmin, tmax, parameter, energy_type):
+    def __init__(self, tmin, tmax, alpha, parameters, energy_type, max_iteration):
         """The constructor of Simulated Annealing method.
 
         Parameters
         ----------
-        method      : str
-            The method to run with simulated annealing.
-        tmin        : float
+        tmin            : float
             Minimum temperature.
-        tmax        : float
+        tmax            : float
             Maximum temperature.
-        parameter   : dict
-            Dictionary of parameter. Key: parameter, value: list.
-        energy_type      : str
+        alpha           : float
+            Cooling factor. Tnew = alpha * Tcurrent
+        parameters      : dict
+            Dictionary of parameters. Key: parameters, value: list.
+        energy_type     : str
             Objective function of simulated annealing. We use internal evaluation for graph clustering.
+        max_iteration   : int
+            Maximum iteration for simulated annealing.
         """
-        self.method = method
         self.Tmin = tmin
         self.Tmax = tmax
-        self.parameter = parameter
+        self.alpha = alpha
+        self.parameters = parameters
         self.energy_type = energy_type
+        self.max_iteration = max_iteration
 
-    def __get_parameter(self):
-        chosen_parameter = {}
-        for param, value in self.parameter.iteritems():
-            chosen_parameter[param] = choice(value)
+    def get_parameter(self):
+        """Get random parameter based on given range.
 
-        return chosen_parameter
+        Returns
+        -------
+        random_parameter    : dict[str, float]
+            Dictionary of random parameters.
+        """
+        random_parameter = {}
+        for param, value in self.parameters.iteritems():
+            random_parameter[param] = choice(value)
 
-    def __run_method(self):
-        if self.method == 'max_clique':
-            pass
+        return random_parameter
 
-    def __get_energy(self, graph, clusters):
+    def get_temperature(self, current_temperature):
+        new_temperature = self.alpha * current_temperature
+        return new_temperature
+
+    def get_energy(self, graph, clusters):
         energy = 0.
         if self.energy_type == 'silhoutte':
             energy = InternalEvaluation.get_silhoutte_index(graph, clusters)
