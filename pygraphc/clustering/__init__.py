072a4fea24c8cc11d3f9c4749f93d806b52d0aa9
Hudan Studiawan <studiawan@gmail.com>
2017-04-06 07:50:46 +0800

    Remove StringSimilarity from clustering module

--- pygraphc/clustering/__init__.py
+++ pygraphc/clustering/__init__.py
@@ -1,9 +1,7 @@
-import pygraphc.clustering.StringSimilarity
 import pygraphc.clustering.ClusterUtility
 import pygraphc.clustering.ConnectedComponents
 import pygraphc.clustering.KCliquePercolation
 import pygraphc.clustering.MaxCliquesPercolation
-from pygraphc.clustering.StringSimilarity import *
 from pygraphc.clustering.ClusterUtility import *
 from pygraphc.clustering.ConnectedComponents import *
 from pygraphc.clustering.KCliquePercolation import *
