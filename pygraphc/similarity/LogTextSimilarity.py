08d572f5784df9afff74de626ba4ffb6ffd46908
Hudan Studiawan <studiawan@gmail.com>
2017-05-10 23:28:31 +0800

    Add pytables to LogTextSimilarity

--- pygraphc/similarity/LogTextSimilarity.py
+++ pygraphc/similarity/LogTextSimilarity.py
@@ -13,18 +13,24 @@ class LogTextSimilarity(object):
         dest = Int32Col()
         similarity = Float32Col()
 
-    def __init__(self, logtype, logs):
+    def __init__(self, mode, logtype, logs, h5file=''):
         """The constructor of class LogTextSimilarity.
 
         Parameters
         ----------
+        mode    : str
+            Mode of operation, i.e., text and text-h5
         logtype : str
             Type for event log, e.g., auth, syslog, etc.
         logs    : list
             List of every line of original logs.
+        h5file  : str
+            File name for h5 file to save cosine similarity result.
         """
+        self.mode = mode
         self.logtype = logtype
         self.logs = logs
+        self.h5file = h5file
 
     def get_cosine_similarity(self):
         """Get cosine similarity from a pair of log lines in a file.
@@ -38,27 +44,35 @@ class LogTextSimilarity(object):
         preprocess = PreprocessLog(self.logtype)
         preprocess.preprocess_text(self.logs)
         events = preprocess.events_text
-
-        # h5 configuration for cosine similarity
-        h5cosine_file = open_file('cosine.h5', mode='w', title='Cosine similarity')
-        h5group = h5file.create_group("/", 'cosine_group', 'Cosine similarity group')
-        h5table = h5file.create_table(h5group, 'cosine_table', Cosine, "Cosine similarity table")
-        h5cosine = h5table.row
-
-        # calculate cosine similarity
         cosines_similarity = {}
-        for log_pair in combinations(range(preprocess.loglength), 2):
-            cosines_similarity[log_pair] = StringSimilarity.get_cosine_similarity(events[log_pair[0]]['tf-idf'],
-                                                                                  events[log_pair[1]]['tf-idf'],
-                                                                                  events[log_pair[0]]['length'],
-                                                                                  events[log_pair[1]]['length'])
-            h5cosine['source'] = log_pair[0]
-            h5cosine['dest'] = log_pair[1]
-            h5cosine['similarity'] = cosines_similarity[log_pair]
-            h5cosine.append()
 
-        # write to file and then close
-        h5table.flush()
-        h5cosine_file.close()
+        if self.mode == 'text':
+            # calculate cosine similarity
+            for log_pair in combinations(range(preprocess.loglength), 2):
+                cosines_similarity[log_pair] = StringSimilarity.get_cosine_similarity(events[log_pair[0]]['tf-idf'],
+                                                                                      events[log_pair[1]]['tf-idf'],
+                                                                                      events[log_pair[0]]['length'],
+                                                                                      events[log_pair[1]]['length'])
+                return cosines_similarity
 
-        return cosines_similarity
+        elif self.mode == 'text-h5':
+            # h5 configuration for cosine similarity
+            h5cosine_file = open_file(self.h5file, mode='w', title='Cosine similarity')
+            h5group = h5file.create_group("/", 'cosine_group', 'Cosine similarity group')
+            h5table = h5file.create_table(h5group, 'cosine_table', Cosine, "Cosine similarity table")
+            h5cosine = h5table.row
+
+            # calculate cosine similarity
+            for log_pair in combinations(range(preprocess.loglength), 2):
+                cosines_similarity[log_pair] = StringSimilarity.get_cosine_similarity(events[log_pair[0]]['tf-idf'],
+                                                                                      events[log_pair[1]]['tf-idf'],
+                                                                                      events[log_pair[0]]['length'],
+                                                                                      events[log_pair[1]]['length'])
+                h5cosine['source'] = log_pair[0]
+                h5cosine['dest'] = log_pair[1]
+                h5cosine['similarity'] = cosines_similarity[log_pair]
+                h5cosine.append()
+
+            # write to file and then close
+            h5table.flush()
+            h5cosine_file.close()
