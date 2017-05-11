4d20fb6757be4086b42e0be855bd7ef121304faa
Hudan Studiawan <studiawan@gmail.com>
2017-05-11 22:53:41 +0800

    Write using pytables is perfect and very fast

--- pygraphc/similarity/LogTextSimilarity.py
+++ pygraphc/similarity/LogTextSimilarity.py
@@ -2,17 +2,13 @@ from pygraphc.preprocess.PreprocessLog import PreprocessLog
 from pygraphc.similarity.StringSimilarity import StringSimilarity
 from itertools import combinations
 from tables import *
+import operator as op
 
 
 class LogTextSimilarity(object):
     """A class for calculating cosine similarity between a log pair. This class is intended for
        non-graph based clustering method.
     """
-    class Cosine(IsDescription):
-        source = Int32Col()
-        dest = Int32Col()
-        similarity = Float32Col()
-
     def __init__(self, mode, logtype, logs, h5file=''):
         """The constructor of class LogTextSimilarity.
 
@@ -31,6 +27,35 @@ class LogTextSimilarity(object):
         self.logtype = logtype
         self.logs = logs
         self.h5file = h5file
+
+    @staticmethod
+    def __ncr(n, r):
+        """Calculate total of combinations nCr.
+
+        Parameters
+        ----------
+        n   : float
+            Numerator.
+        r   : float
+            Denumerator.
+
+        Returns
+        -------
+        total   : float
+            Total combinations.
+
+        Notes
+        -----
+        http://stackoverflow.com/questions/4941753/is-there-a-math-ncr-function-in-python
+        """
+        r = min(r, n - r)
+        if r == 0:
+            return 1
+
+        numer = reduce(op.mul, xrange(n, n - r, -1))
+        denom = reduce(op.mul, xrange(1, r + 1))
+        total = numer // denom
+        return total
 
     def get_cosine_similarity(self):
         """Get cosine similarity from a pair of log lines in a file.
@@ -53,17 +78,26 @@ class LogTextSimilarity(object):
                                                                                       events[log_pair[1]]['tf-idf'],
                                                                                       events[log_pair[0]]['length'],
                                                                                       events[log_pair[1]]['length'])
-                return cosines_similarity
+            return cosines_similarity
 
         elif self.mode == 'text-h5':
             # h5 configuration for cosine similarity
+            class Cosine(IsDescription):
+                source = Int32Col()
+                dest = Int32Col()
+                similarity = Float32Col()
+
+            # get combinations
+            total_combination = self.__ncr(preprocess.loglength, 2)
+            h5filter = Filters(complib='zlib', complevel=1)
             h5cosine_file = open_file(self.h5file, mode='w', title='Cosine similarity')
-            h5group = h5file.create_group("/", 'cosine_group', 'Cosine similarity group')
-            h5table = h5file.create_table(h5group, 'cosine_table', Cosine, "Cosine similarity table")
+            h5group = h5cosine_file.create_group("/", 'cosine_group', 'Cosine similarity group')
+            h5table = h5cosine_file.create_table(h5group, 'cosine_table', Cosine, "Cosine similarity table",
+                                                 filters=h5filter, expectedrows=total_combination)
             h5cosine = h5table.row
 
             # calculate cosine similarity
-            for log_pair in combinations(range(preprocess.loglength), 2):
+            for log_pair in combinations(xrange(preprocess.loglength), 2):
                 cosines_similarity[log_pair] = StringSimilarity.get_cosine_similarity(events[log_pair[0]]['tf-idf'],
                                                                                       events[log_pair[1]]['tf-idf'],
                                                                                       events[log_pair[0]]['length'],
@@ -74,5 +108,7 @@ class LogTextSimilarity(object):
                 h5cosine.append()
 
             # write to file and then close
+            h5table.cols.source.create_csindex()
+            h5table.cols.dest.create_csindex()
             h5table.flush()
             h5cosine_file.close()
