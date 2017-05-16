a3f90fa4b302927b79d407eed956ccf5ed1bddd6
Hudan Studiawan <studiawan@gmail.com>
2017-05-17 05:23:26 +0800

    Add text-csv mode

--- pygraphc/similarity/LogTextSimilarity.py
+++ pygraphc/similarity/LogTextSimilarity.py
@@ -2,61 +2,35 @@ from pygraphc.preprocess.PreprocessLog import PreprocessLog
 from pygraphc.similarity.StringSimilarity import StringSimilarity
 from itertools import combinations
 from tables import *
-import operator as op
 import networkx as nx
+import csv
 
 
 class LogTextSimilarity(object):
     """A class for calculating cosine similarity between a log pair. This class is intended for
        non-graph based clustering method.
     """
-    def __init__(self, mode, logtype, logs, h5file=''):
+    def __init__(self, mode, logtype, logs, clusters, h5file=''):
         """The constructor of class LogTextSimilarity.
 
         Parameters
         ----------
-        mode    : str
+        mode        : str
             Mode of operation, i.e., text and text-h5
-        logtype : str
+        logtype     : str
             Type for event log, e.g., auth, syslog, etc.
-        logs    : list
+        logs        : list
             List of every line of original logs.
-        h5file  : str
+        clusters    : dict
+            Dictionary of clusters. Key: cluster_id, value: list of log line id.
+        h5file      : str
             File name for h5 file to save cosine similarity result.
         """
         self.mode = mode
         self.logtype = logtype
         self.logs = logs
+        self.clusters = clusters
         self.h5file = h5file
-
-    @staticmethod
-    def __ncr(n, r):
-        """Calculate total of combinations nCr.
-
-        Parameters
-        ----------
-        n   : float
-            Numerator.
-        r   : float
-            Denumerator.
-
-        Returns
-        -------
-        total   : float
-            Total combinations.
-
-        Notes
-        -----
-        http://stackoverflow.com/questions/4941753/is-there-a-math-ncr-function-in-python
-        """
-        r = min(r, n - r)
-        if r == 0:
-            return 1
-
-        numer = reduce(op.mul, xrange(n, n - r, -1))
-        denom = reduce(op.mul, xrange(1, r + 1))
-        total = numer // denom
-        return total
 
     def get_cosine_similarity(self):
         """Get cosine similarity from a pair of log lines in a file.
@@ -84,35 +58,61 @@ class LogTextSimilarity(object):
         elif self.mode == 'text-h5':
             # h5 configuration for cosine similarity
             class Cosine(IsDescription):
-                source = Int32Col()
-                dest = Int32Col()
+                # dest = Int32Col()
                 similarity = Float32Col()
 
-            # get combinations
-            total_combination = self.__ncr(preprocess.loglength, 2)
             h5filter = Filters(complib='zlib', complevel=1)
             h5cosine_file = open_file(self.h5file, mode='w', title='Cosine similarity')
-            h5group = h5cosine_file.create_group("/", 'cosine_group', 'Cosine similarity group')
-            h5table = h5cosine_file.create_table(h5group, 'cosine_table', Cosine, "Cosine similarity table",
-                                                 filters=h5filter, expectedrows=total_combination)
-            h5cosine = h5table.row
 
-            # calculate cosine similarity
-            for log_pair in combinations(xrange(preprocess.loglength), 2):
-                cosines_similarity[log_pair] = StringSimilarity.get_cosine_similarity(events[log_pair[0]]['tf-idf'],
-                                                                                      events[log_pair[1]]['tf-idf'],
-                                                                                      events[log_pair[0]]['length'],
-                                                                                      events[log_pair[1]]['length'])
-                h5cosine['source'] = log_pair[0]
-                h5cosine['dest'] = log_pair[1]
-                h5cosine['similarity'] = cosines_similarity[log_pair]
-                h5cosine.append()
+            for nodex in xrange(preprocess.loglength):
+                # set h5 table
+                h5group = h5cosine_file.create_group('/', 'group' + str(nodex), 'Cosine similarity group')
 
-            # write to file and then close
-            h5table.cols.source.create_csindex()
-            h5table.cols.dest.create_csindex()
-            h5table.flush()
+                # calculate cosine similarity
+                for cluster_id, cluster in self.clusters.iteritems():
+                    cosines_similarity.clear()
+                    for c in cluster:
+                        if nodex != c:
+                            similarity = StringSimilarity.get_cosine_similarity(events[nodex]['tf-idf'],
+                                                                                events[c]['tf-idf'],
+                                                                                events[nodex]['length'],
+                                                                                events[c]['length'])
+                            if similarity > 0:
+                                cosines_similarity[c] = similarity
+
+                    if cosines_similarity:
+                        # write to file and then close
+                        h5table = h5cosine_file.create_table(h5group, 'table' + str(cluster_id), Cosine,
+                                                             'Cosine similarity table', filters=h5filter)
+                        h5cosine = h5table.row
+                        for dest, sim in cosines_similarity.iteritems():
+                            # h5cosine['dest'] = dest
+                            h5cosine['similarity'] = sim
+                            h5cosine.append()
+                        h5table.flush()
             h5cosine_file.close()
+
+        elif self.mode == 'text-csv':
+            print self.mode
+            for nodex in xrange(preprocess.loglength):
+                csv_file = '/tmp/cosine-' + str(nodex) + '.csv'
+                f = open(csv_file, 'wb')
+                writer = csv.writer(f)
+
+                for cluster_id, cluster in self.clusters.iteritems():
+                    row = []
+                    for c in cluster:
+                        if nodex != c:
+                            similarity = StringSimilarity.get_cosine_similarity(events[nodex]['tf-idf'],
+                                                                                events[c]['tf-idf'],
+                                                                                events[nodex]['length'],
+                                                                                events[c]['length'])
+                            # if similarity > 0:
+                            row.append(1 - similarity)
+                    if row:
+                        row.append(cluster_id)
+                        writer.writerow(row)
+                f.close()
 
     def get_events(self):
         preprocess = PreprocessLog(self.logtype)
