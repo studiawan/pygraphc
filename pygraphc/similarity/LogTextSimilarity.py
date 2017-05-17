33f591b65a1f721caf2259b3290ae26f706a710c
Hudan Studiawan <studiawan@gmail.com>
2017-05-17 11:25:00 +0800

    Add multiprocessing support to LogTextSimilarity

--- pygraphc/similarity/LogTextSimilarity.py
+++ pygraphc/similarity/LogTextSimilarity.py
@@ -1,15 +1,15 @@
 from pygraphc.preprocess.PreprocessLog import PreprocessLog
 from pygraphc.similarity.StringSimilarity import StringSimilarity
 from itertools import combinations
-import networkx as nx
 import csv
+import multiprocessing
 
 
 class LogTextSimilarity(object):
     """A class for calculating cosine similarity between a log pair. This class is intended for
        non-graph based clustering method.
     """
-    def __init__(self, mode, logtype, logs, clusters, h5file=''):
+    def __init__(self, mode, logtype, logs, clusters):
         """The constructor of class LogTextSimilarity.
 
         Parameters
@@ -22,14 +22,34 @@ class LogTextSimilarity(object):
             List of every line of original logs.
         clusters    : dict
             Dictionary of clusters. Key: cluster_id, value: list of log line id.
-        h5file      : str
-            File name for h5 file to save cosine similarity result.
         """
         self.mode = mode
         self.logtype = logtype
         self.logs = logs
         self.clusters = clusters
-        self.h5file = h5file
+        self.events = {}
+
+    def write_cosine_csv(self, node):
+        csv_file = '/tmp/cosine-' + str(node) + '.csv'
+        f = open(csv_file, 'wb')
+        writer = csv.writer(f)
+        for cluster_id, cluster in self.clusters.iteritems():
+            row = []
+            for c in cluster:
+                if node != c:
+                    similarity = StringSimilarity.get_cosine_similarity(self.events[node]['tf-idf'],
+                                                                        self.events[c]['tf-idf'],
+                                                                        self.events[node]['length'],
+                                                                        self.events[c]['length'])
+                    if similarity > 0:
+                        row.append(1 - similarity)
+            if row:
+                row.append(cluster_id)
+                writer.writerow(row)
+        f.close()
+
+    def __call__(self, node):
+        return self.write_cosine_csv(node)
 
     def get_cosine_similarity(self):
         """Get cosine similarity from a pair of log lines in a file.
@@ -42,36 +62,23 @@ class LogTextSimilarity(object):
         """
         preprocess = PreprocessLog(self.logtype)
         preprocess.preprocess_text(self.logs)
-        events = preprocess.events_text
+        self.events = preprocess.events_text
         cosines_similarity = {}
 
         if self.mode == 'text':
             # calculate cosine similarity
             for log_pair in combinations(range(preprocess.loglength), 2):
-                cosines_similarity[log_pair] = StringSimilarity.get_cosine_similarity(events[log_pair[0]]['tf-idf'],
-                                                                                      events[log_pair[1]]['tf-idf'],
-                                                                                      events[log_pair[0]]['length'],
-                                                                                      events[log_pair[1]]['length'])
+                cosines_similarity[log_pair] = \
+                    StringSimilarity.get_cosine_similarity(self.events[log_pair[0]]['tf-idf'],
+                                                           self.events[log_pair[1]]['tf-idf'],
+                                                           self.events[log_pair[0]]['length'],
+                                                           self.events[log_pair[1]]['length'])
             return cosines_similarity
 
         elif self.mode == 'text-csv':
             print self.mode
-            for nodex in xrange(preprocess.loglength):
-                csv_file = '/tmp/cosine-' + str(nodex) + '.csv'
-                f = open(csv_file, 'wb')
-                writer = csv.writer(f)
-
-                for cluster_id, cluster in self.clusters.iteritems():
-                    row = []
-                    for c in cluster:
-                        if nodex != c:
-                            similarity = StringSimilarity.get_cosine_similarity(events[nodex]['tf-idf'],
-                                                                                events[c]['tf-idf'],
-                                                                                events[nodex]['length'],
-                                                                                events[c]['length'])
-                            # if similarity > 0:
-                            row.append(1 - similarity)
-                    if row:
-                        row.append(cluster_id)
-                        writer.writerow(row)
-                f.close()
+            nodes = range(preprocess.loglength)
+            pool = multiprocessing.Pool(processes=4)
+            pool.map(self, nodes)
+            pool.close()
+            pool.join()
