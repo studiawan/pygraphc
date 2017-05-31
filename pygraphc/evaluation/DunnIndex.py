0ffc76c6fc42bfe107b53fa0973b2932ec257486
Hudan Studiawan <studiawan@gmail.com>
2017-05-31 08:38:25 +0800

    Add cosine_file to DunnIndex class

--- pygraphc/evaluation/DunnIndex.py
+++ pygraphc/evaluation/DunnIndex.py
@@ -3,9 +3,10 @@ import numpy as np
 
 
 class DunnIndex(object):
-    def __init__(self, mode, clusters):
+    def __init__(self, mode, clusters, cosine_file):
         self.mode = mode
         self.clusters = clusters
+        self.cosine_file = cosine_file
 
     def __get_node_distance(self, measurement, intra_cluster, source):
         if self.mode == 'text-csv':
@@ -13,7 +14,7 @@ class DunnIndex(object):
             final_distance = 0.
 
             # open csv file
-            csv_file = '/tmp/cosine-' + str(source_node) + '.csv'
+            csv_file = self.cosine_file + str(source_node) + '.csv'
             with open(csv_file, 'rb') as f:
                 reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)
 
