66e368edd6514ad6b0c057b5f82b1eb073a62d3d
Hudan Studiawan <studiawan@gmail.com>
2017-06-29 17:44:15 +0800

    Add docs in SilhouetteIndex class

--- pygraphc/evaluation/SilhouetteIndex.py
+++ pygraphc/evaluation/SilhouetteIndex.py
@@ -3,20 +3,45 @@ import numpy as np
 
 
 class SilhouetteIndex(object):
+    """Class for calculating Silhouette Index for cluster internal evaluation.
+    """
     def __init__(self, mode, clusters, cosine_file):
-        """Calculate Silhouette index for cluster internal evaluation.
+        """Constructor of class SilhouetteIndex
 
         Parameters
         ----------
         mode        : str
+            Mode of clustering method, i.e., graph, text, text-csv.
         clusters    : dict
+            A dictionary containing node identifier per cluster. Key: cluster identifier,
+            value: list of node identifier.
         cosine_file : str
+            Filename that contains cosine similarity of a node to other nodes per cluster.
         """
         self.mode = mode
         self.clusters = clusters
         self.cosine_file = cosine_file
 
     def __get_node_distance(self, measurement, intra_cluster, source):
+        """Get distance from a node to its neighbor.
+
+        The neighbors can be located in intra-cluster or inter-cluster. The distance means
+        edge weight in the graph case. In non-graph clustering method, node is equal with log line id.
+
+        Parameters
+        ----------
+        measurement     : str
+            Mode of measurement, i.e., min, max, avg.
+        intra_cluster   : bool
+            True: intra-cluster, False: inter-cluster.
+        source          : dict
+            Source node and source cluster.
+
+        Returns
+        -------
+        final_distance  : float
+            Final distance for a node to intra or inter-cluster.
+        """
         if self.mode == 'text-csv':
             source_node, source_cluster = source['source_node'], source['source_cluster']
             final_distance = 0.
@@ -68,6 +93,13 @@ class SilhouetteIndex(object):
             return round(final_distance, 3)
 
     def __get_node_silhouette(self):
+        """Get node silhoutte.
+
+        Returns
+        -------
+        node_silhouttes : dict[int, float]
+            A dictionary containing silhoutte per node. Key: node identifier, value: silhoutte.
+        """
         node_silhouettes = {}
         for cluster_id, cluster in self.clusters.iteritems():
             if len(cluster) == 1:
@@ -87,6 +119,13 @@ class SilhouetteIndex(object):
         return node_silhouettes
 
     def __get_cluster_silhouette(self):
+        """Get cluster silhoutte.
+
+        Returns
+        -------
+        cluster_silhouttes  : dict[int, float]
+            A dictionary containing silhoutte per cluster. Key: cluster identifier, value: silhoutte.
+        """
         node_silhouettes, cluster_silhouettes = {}, {}
         if self.mode == 'text-csv':
             node_silhouettes = self.__get_node_silhouette()
@@ -100,6 +139,20 @@ class SilhouetteIndex(object):
         return cluster_silhouettes
 
     def get_silhouette_index(self):
+        """Get silhoutte index for a graph [Almeida2011]_.
+
+        Returns
+        -------
+        silhoutte_index : float
+            The silhoutte index for a graph.
+
+        References
+        ----------
+        .. [Almeida2011] Helio Almeida, Dorgival Guedes, Wagner Meira Jr, and Mohammed J. Zaki.
+                         "Is there a best quality metric for graph clusters?."
+                         In Joint European Conference on Machine Learning and Knowledge Discovery in Databases,
+                         pp. 44-59. Springer Berlin Heidelberg, 2011.
+        """
         cluster_silhouettes = {}
         if self.mode == 'text-csv':
             cluster_silhouettes = self.__get_cluster_silhouette()
