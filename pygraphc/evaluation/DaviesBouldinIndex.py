be0c828f98a704f44cbb5681e94f6712f76bc2a7
Hudan Studiawan <studiawan@gmail.com>
2017-07-06 10:35:26 +0800

    Add docs to DaviesBouldinIndex class

--- pygraphc/evaluation/DaviesBouldinIndex.py
+++ pygraphc/evaluation/DaviesBouldinIndex.py
@@ -4,7 +4,25 @@ from itertools import combinations, product
 
 
 class DaviesBouldinIndex(object):
+    """A class to calculate Davies-Bouldin index [Davies1979]_. We use the terms for this index from [Kovacs2005]_.
+
+    References
+    ----------
+    .. [Davies1979] Davies, D. L., & Bouldin, D. W. A cluster separation measure.
+                    IEEE Transactions on Pattern Analysis and Machine Intelligence, (2), 224-227, 1979.
+    .. [Kovacs2005] Kovacs, F., Legany, C., & Babos, A. Cluster validity measurement techniques.
+                    In 6th International Symposium of Hungarian Researchers on Computational Intelligence, 2005.
+    """
     def __init__(self, graph, clusters):
+        """Constructor for DaviesBouldinIndex class.
+
+        Parameters
+        ----------
+        graph       : graph
+            A graph to be evaluated.
+        clusters    : dict
+            Dictionary containing the cluster data. Key: cluster id, value: list of nodes.
+        """
         self.graph = graph
         self.clusters = clusters
         self.cluster_centroids = {}
@@ -16,6 +34,18 @@ class DaviesBouldinIndex(object):
         self.__get_all_cluster_properties()
 
     def __get_centroid(self, cluster=None):
+        """Get the centroid of a cluster in the graph or the centroid of the graph.
+
+        Parameters
+        ----------
+        cluster : list
+            Nodes list of a cluster.
+
+        Returns
+        -------
+        centroid_node   : int
+            Centroid node of a cluster.
+        """
         # centroid of a cluster
         if cluster:
             subgraph = self.graph.subgraph(cluster)
@@ -29,6 +59,8 @@ class DaviesBouldinIndex(object):
         return centroid_node
 
     def __get_all_cluster_properties(self):
+        """Get cluster properties, i.e., centroid, total nodes per cluster, and total nodes in a graph.
+        """
         # get cluster centroid and cluster total nodes
         for cluster_id, nodes in self.clusters.iteritems():
             self.cluster_centroids[cluster_id] = self.__get_centroid(nodes)
@@ -37,6 +69,20 @@ class DaviesBouldinIndex(object):
         self.total_nodes = self.graph.number_of_nodes()
 
     def __get_distance(self, source, dest):
+        """Get distance from source to destination using Dijkstra algorithm.
+
+        Parameters
+        ----------
+        source  : int
+            Source node.
+        dest    : int
+            Destination node.
+
+        Returns
+        -------
+        distance    : float
+            Distance from source node to destination node.
+        """
         # get distance from source to destination
         try:
             distance = nx.dijkstra_path_length(self.graph, source, dest)
@@ -46,6 +92,13 @@ class DaviesBouldinIndex(object):
         return distance
 
     def __get_dispersion(self):
+        """Get dispersion measure of a cluster (s). It is actually a within-cluster or intra-cluster similarity measure.
+
+        Returns
+        -------
+        cluster_dispersions : dict
+            Cluster dispersion. Key: cluster id, value: dispersion.
+        """
         cluster_dispersions = {}
         for cluster_id, nodes in self.clusters.iteritems():
             distances = []
@@ -58,6 +111,13 @@ class DaviesBouldinIndex(object):
         return cluster_dispersions
 
     def __get_dissimilarity(self):
+        """Get cluster dissimilarity measure (d). It is actually a between-cluster or inter-cluster similarity measure.
+
+        Returns
+        -------
+        cluster_dissimilarity   : dict
+            Cluster dissimilarity. Key: cluster id, value: dissimilarity.
+        """
         cluster_dissimilarity = {}
         for cluster_id1, cluster_id2 in combinations(xrange(self.total_cluster), 2):
             cluster_dissimilarity[(cluster_id1, cluster_id2)] = self.__get_distance(self.cluster_centroids[cluster_id1],
@@ -65,6 +125,13 @@ class DaviesBouldinIndex(object):
         return cluster_dissimilarity
 
     def __get_similarity(self):
+        """Get similarity measure of all clusters (R_ij).
+
+        Returns
+        -------
+        similarity  : dict
+            Cluster similarity. Key: cluster id, value: similarity.
+        """
         similarity = {}
         cluster_dispersions = self.__get_dispersion()
         cluster_dissimilarity = self.__get_dissimilarity()
@@ -76,6 +143,13 @@ class DaviesBouldinIndex(object):
         return similarity
 
     def __get_r(self):
+        """Get maximum similarity measure for each cluster.
+
+        Returns
+        -------
+        r   : dict
+            Maximum similarity value for each cluster. Key: cluster_id, value: maximum similarity.
+        """
         r = {}
         similarity = self.__get_similarity()
         similarity_keys = similarity.keys()
@@ -92,6 +166,13 @@ class DaviesBouldinIndex(object):
         return r
 
     def get_davies_bouldin(self):
+        """Get Davies-Bouldin index.
+
+        Returns
+        -------
+        db_index    : float
+            Davies-Bouldin index.
+        """
         r = self.__get_r()
         db_index = 1 / self.total_cluster * sum(r.values())
 
