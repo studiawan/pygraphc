4e40ff667352cc9ba5bba0d38389c1b3d7a83bc2
Hudan Studiawan <studiawan@gmail.com>
2017-07-21 19:56:15 +0800

    Edit to connect with parallel preprocess

--- pygraphc/similarity/JaroWinkler.py
+++ pygraphc/similarity/JaroWinkler.py
@@ -1,46 +1,32 @@
 import jellyfish
 import multiprocessing
 from itertools import combinations
-from time import time
 
 
 class JaroWinkler(object):
-    def __init__(self, logs, log_length):
-        self.logs = logs
-        self.log_length = log_length
+    def __init__(self, event_attributes, event_length):
+        self.event_attributes = event_attributes
+        self.event_length = event_length
 
-    def __jarowinkler(self, line):
-        string1 = unicode(self.logs[line[0]], 'utf-8')
-        string2 = unicode(self.logs[line[1]], 'utf-8')
+    def __jarowinkler(self, unique_event_id):
+        string1 = unicode(self.event_attributes[unique_event_id[0]]['preprocessed_event'], 'utf-8')
+        string2 = unicode(self.event_attributes[unique_event_id[1]]['preprocessed_event'], 'utf-8')
         return jellyfish.jaro_winkler(string1, string2)
 
-    def __call__(self, line):
-        return self.__jarowinkler(line)
+    def __call__(self, unique_event_id):
+        distance = self.__jarowinkler(unique_event_id)
+        if distance > 0:
+            distance_with_id = (unique_event_id[0], unique_event_id[1], distance)
+            return distance_with_id
 
     def get_jarowinkler(self):
-        # get line number combination
-        line_combination = list(combinations(xrange(self.log_length), 2))
+        # get unique event id combination
+        event_id_combination = list(combinations(xrange(self.event_length), 2))
 
         # get distance with multiprocessing
         pool = multiprocessing.Pool(processes=4)
-        distances = pool.map(self, line_combination)
+        distances = pool.map(self, event_id_combination)
         pool.close()
         pool.join()
 
         return distances
-
-# open file
-start = time()
-log_file = '/home/hs32832011/Git/labeled-authlog/dataset/Hofstede2014/dataset1_perday/Dec 1.log'
-with open(log_file, 'r') as f:
-    lines = f.readlines()
-log_length = len(lines)
-
-jw = JaroWinkler(lines, log_length)
-dist = jw.get_jarowinkler()
-
-# print runtime
-duration = time() - start
-minute, second = divmod(duration, 60)
-hour, minute = divmod(minute, 60)
-print "Runtime: %d:%02d:%02d" % (hour, minute, second)
