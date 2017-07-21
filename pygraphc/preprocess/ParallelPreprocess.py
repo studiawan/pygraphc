82c0a8ca059ddbb1320c55e0208951b5a093a1b3
Hudan Studiawan <studiawan@gmail.com>
2017-07-21 16:29:39 +0800

    Edit parallel preprocess that feeds attribute to networkx

--- pygraphc/preprocess/ParallelPreprocess.py
+++ pygraphc/preprocess/ParallelPreprocess.py
@@ -9,6 +9,7 @@ class ParallelPreprocess(object):
         self.log_file = log_file
         self.logs = []
         self.log_length = 0
+        self.unique_events = []
 
     def __call__(self, line):
         return self.__get_events(line)
@@ -21,7 +22,8 @@ class ParallelPreprocess(object):
         self.log_length = len(self.logs)
 
     @staticmethod
-    def __get_events(line):
+    def __get_events(logs_with_id):
+        log_index, line = logs_with_id
         line = sub('[^a-zA-Z]', ' ', line)
         line = line.replace('_', ' ')
 
@@ -39,23 +41,47 @@ class ParallelPreprocess(object):
         stopwords = corpus.stopwords.words('english')
         stopwords_result = [w.lower() for w in line.split() if w.lower() not in stopwords]
         preprocessed_events = ' '.join(stopwords_result)
+        preprocessed_with_id = (log_index, preprocessed_events)
 
-        return preprocessed_events
+        return preprocessed_with_id
 
     def get_unique_events(self):
         self.__read_log()
+        logs_with_id = []
+        for index, log in enumerate(self.logs):
+            logs_with_id.append((index, log))
 
         pool = multiprocessing.Pool(processes=4)
-        events = pool.map(self, self.logs)
+        events = pool.map(self, logs_with_id)
         pool.close()
         pool.join()
-        unique_events = set(events)
 
-        return unique_events
+        # get graph attributes
+        unique_events_only = {}
+        unique_event_id = 0
+        attributes = {}
+        for log_id, event in events:
+            if event not in unique_events_only.values():
+                unique_events_only[unique_event_id] = event
+                attributes[unique_event_id] = {'preprocessed_event': event,
+                                               'cluster': unique_event_id,
+                                               'member': [log_id]}
+                unique_event_id += 1
+            else:
+                for index, attr in attributes.iteritems():
+                    if event == attr['preprocessed_event']:
+                        attr['member'].append(log_id)
+
+        # get unique events for networkx
+        for index, attr in attributes.iteritems():
+            self.unique_events.append((index, attr))
+            print (index, attr)
+
+        return self.unique_events
 
 # open file
 start = time()
-logfile = '/home/hudan/Git/labeled-authlog/dataset/SecRepo/auth-perday/dec-2.log'
+logfile = '/home/hudan/Git/labeled-authlog/dataset/SecRepo/auth-perday/dec-1.log'
 
 # preprocess
 p = ParallelPreprocess(logfile)
