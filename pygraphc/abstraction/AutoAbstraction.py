38d8a3a40e03402c22470613c5ca636e84b4e93b
Hudan Studiawan <studiawan@gmail.com>
2017-10-21 18:09:02 +0800

    Edit dictionary structure for abstraction

--- pygraphc/abstraction/AutoAbstraction.py
+++ pygraphc/abstraction/AutoAbstraction.py
@@ -25,31 +25,38 @@ class AutoAbstraction(object):
             # we need OrderedSet to preserve order because regular set does not preserve order
             group_keys = self.count_groups.keys()
             if words_count not in group_keys:
-                self.count_groups[words_count] = []
-            self.count_groups[words_count].append(OrderedSet(words_split))
+                self.count_groups[words_count] = {}
+            self.count_groups[words_count][event_id] = OrderedSet(words_split)
 
     def get_abstraction(self):
+        # group messages which has the same word count
         self.__get_count_groups()
 
         # get common words with intersection as abstraction
         abstraction_id = 0
         for words_count, group in self.count_groups.iteritems():
-            group_length = len(group)
+            group_length = len(group.values())
             if group_length > 1:
-                self.abstractions[abstraction_id] = OrderedSet(group[0])
-                for message in group:
-                    self.abstractions[abstraction_id].intersection_update(message)
+                # get abstraction
+                # prevent initialization to refer to current group variable. re-initialize with OrderedSet()
+                self.abstractions[abstraction_id] = {'original_id': [],
+                                                     'abstraction': OrderedSet(group.values()[0])}
+                for index, message in group.iteritems():
+                    self.abstractions[abstraction_id]['original_id'].append(index)
+                    self.abstractions[abstraction_id]['abstraction'].intersection_update(message)
 
                 # check for abstraction that only contains one word
-                if len(self.abstractions[abstraction_id]) == 1:
-                    for message in group:
-                        self.abstractions[abstraction_id] = message
+                if len(self.abstractions[abstraction_id]['abstraction']) == 1:
+                    for index, message in group.iteritems():
+                        self.abstractions[abstraction_id] = {'original_id': [index],
+                                                             'abstraction': message}
                         abstraction_id += 1
                 else:
                     abstraction_id += 1
 
             elif group_length == 1:
-                self.abstractions[abstraction_id] = group[0]
+                self.abstractions[abstraction_id] = {'original_id': [group.keys()[0]],
+                                                     'abstraction': group.values()[0]}
                 abstraction_id += 1
 
 aa = AutoAbstraction('/home/hudan/Git/labeled-authlog/dataset/illustration/per_day/dec-15.log')
