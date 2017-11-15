7b78dc5e7d90eeec9071401fb264427ebc45ebc9
Hudan Studiawan <studiawan@gmail.com>
2017-11-15 19:47:59 +0800

    Add configuration script and get methods

--- pygraphc/experiment/Experiment.py
+++ pygraphc/experiment/Experiment.py
@@ -1,15 +1,18 @@
 from ConfigParser import SafeConfigParser
 import os
 import fnmatch
+# import csv
 
 
 class Experiment(object):
     def __init__(self, method):
         self.method = method
         self.configuration = {}
+        self.methods = {}
+        self.files = {}
 
     def __read_config(self):
-        # read configuration file to run an experiment based on a specific mathod and a dataset
+        # read configuration file to run an experiment based on a specific method and a dataset
         parser = SafeConfigParser()
         current_path = os.path.dirname(os.path.realpath(__file__))
         config_path = os.path.join(current_path, 'config', self.method + '.conf')
@@ -32,31 +35,59 @@ class Experiment(object):
                 full_path = os.path.join(root, filename)
                 matches.append((full_path, filename))
 
-        files = {}
         result_path = self.configuration['experiment_result_path']['result_path']
         for full_path, filename in matches:
-            files[filename] = {
+            self.files[filename] = {
                 'result_path': result_path,
-                'log_path': full_path,
-                'result_percluster': result_path + filename + '.percluster',
-                'result_perline': result_path + filename + '.perline',
+                'log_path': full_path
             }
 
         if self.configuration['main']['clustering']:
             for full_path, filename in matches:
+                # get evaluation file for clustering
+                self.files[filename]['evaluation_file'] = \
+                    os.path.join(result_path, dataset, self.configuration['clustering_evaluation']['evaluation_file'])
+
+                # get all files for clustering
                 properties = {}
-                for key, value in self.configuration['clustering_result_path']:
-                    properties[key] = os.path.join(result_path, filename, value)
+                for key, value in self.configuration['clustering_result_path'].iteritems():
+                    properties[key] = os.path.join(result_path, dataset, value, filename)
 
-                # concat two dictionaries
-                files[filename] = {
+                # update self.files dictionaries
+                self.files[filename].update(properties)
 
-                }
+        for k, v in self.files.iteritems():
+            print k, v
+
+    def __get_methods(self):
+        parser = SafeConfigParser()
+        current_path = os.path.dirname(os.path.realpath(__file__))
+        config_path = os.path.join(current_path, 'config', 'method.conf')
+        parser.read(config_path)
+
+        for section_name in parser.sections():
+            options = {}
+            methods = []
+            for name, value in parser.items(section_name):
+                options[name] = value
+                methods = options[name].split('\n')
+            self.methods[section_name] = methods
 
     def run_experiment(self):
+        self.__get_methods()
         self.__read_config()
         self.__get_dataset()
 
+        # # open evaluation file
+        # f = open(self.files['evaluation_file'], 'wt')
+        # writer = csv.writer(f)
+        #
+        # # set header for evaluation file
+        # header = self.configuration['clustering_evaluation']['evaluation_file_header']
+        # writer.writerow(header)
+        #
+        # f.close()
 
-e = Experiment('EMCSA')
+
+e = Experiment('max_clique_weighted_sa')
 e.run_experiment()
