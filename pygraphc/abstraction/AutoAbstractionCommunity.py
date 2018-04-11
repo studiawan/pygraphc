f925d334fe052e378087d3fe5d7eec49daadcd56
Hudan Studiawan <studiawan@gmail.com>
2018-04-11 22:54:17 +0800

    Triple level down of community detection

--- pygraphc/abstraction/AutoAbstractionCommunity.py
+++ pygraphc/abstraction/AutoAbstractionCommunity.py
@@ -12,6 +12,7 @@ class AutoAbstraction(object):
         self.clusters = {}
         self.abstraction_candidates = {}
         self.abstractions = {}
+        self.previous_clusters = {}
 
     def __get_community_detection(self):
         # create graph
@@ -19,13 +20,49 @@ class AutoAbstraction(object):
         self.graph = graph_model.create_graph()
         self.graph_noattributes = graph_model.create_graph_noattributes()
 
+        # start of phase 1
         # write to file
-        gexf_file = os.path.join('/tmp/', self.log_file + '.gexf')
+        gexf_file = os.path.join('/', 'tmp', self.log_file.split('/')[-1] + '.gexf')
         nx.write_gexf(self.graph_noattributes, gexf_file)
 
         # graph clustering based on Louvan community detection
         louvan = Louvan(gexf_file)
-        self.clusters = louvan.get_cluster()
+        temporary_clusters = louvan.get_cluster()
+
+        # start of phase 2
+        cluster_id = 0
+        for temporary_index, temporary_cluster in temporary_clusters.iteritems():
+            # write to file
+            gexf_file = os.path.join('/', 'tmp', self.log_file.split('/')[-1] + '.gexf')
+            temporary_cluster = [int(node) for node in temporary_cluster]
+            nx.write_gexf(self.graph_noattributes.subgraph(temporary_cluster), gexf_file)
+
+            # graph clustering based on Louvan community detection
+            louvan = Louvan(gexf_file)
+            temporary_clusters2 = louvan.get_cluster()
+
+            if len(temporary_clusters2.keys()) == 1:
+                self.clusters[cluster_id] = temporary_clusters2.values()[0]
+                cluster_id += 1
+            else:
+                # start of phase 3
+                for temporary_index2, temporary_cluster2 in temporary_clusters2.iteritems():
+                    # write to file
+                    gexf_file = os.path.join('/', 'tmp', self.log_file.split('/')[-1] + '.gexf')
+                    temporary_cluster2 = [int(node) for node in temporary_cluster2]
+                    nx.write_gexf(self.graph_noattributes.subgraph(temporary_cluster2), gexf_file)
+
+                    # graph clustering based on Louvan community detection
+                    louvan = Louvan(gexf_file)
+                    temporary_clusters3 = louvan.get_cluster()
+
+                    if len(temporary_clusters3.keys()) == 1:
+                        self.clusters[cluster_id] = temporary_clusters3.values()[0]
+                        cluster_id += 1
+                    else:
+                        for temporary_index3, temporary_cluster3 in temporary_clusters3.iteritems():
+                            self.clusters[cluster_id] = temporary_cluster3
+                            cluster_id += 1
 
     def __get_count_groups(self):
         abstraction_id = 0
@@ -71,12 +108,10 @@ class AutoAbstraction(object):
                         # set original line id
                         self.abstractions[abstraction_id]['original_id'].extend(self.graph.node[index]['member'])
 
-                        # check for abstraction that only contains one word, or
-                        # if abstraction only contains asterisks, then
+                        # check for abstraction that only contains one word
                         # the abstraction is its original message in count group
-                        if (len(self.abstractions[abstraction_id]['abstraction']) == 1) or \
-                                (set(self.abstractions[abstraction_id]['abstraction']) == {'*'}):
-                            self.abstractions[abstraction_id]['message'] = message
+                        if len(self.abstractions[abstraction_id]['abstraction']) == 1:
+                            self.abstractions[abstraction_id]['abstraction'] = message
 
                 elif candidate_length == 1:
                     node_id = candidate.keys()[0]
@@ -84,15 +119,32 @@ class AutoAbstraction(object):
                                                          'abstraction': candidate.values()[0]}
                 abstraction_id += 1
 
+    def __check_asterisk_only(self, abstraction_id, abstraction_list):
+        # if abstraction only contains asterisks, each candidate becomes an abstraction
+        if set(abstraction_list) == set('*'):
+            print self.abstractions[abstraction_id]
+
     def __check_subabstraction(self):
         # check whether an abstraction is a substring of another abstraction
         pass
+
+    def __print(self):
+        for k, v in self.abstraction_candidates.iteritems():
+            print self.abstractions[k]['abstraction']
+            for x, y in v.iteritems():
+                for p, q in y.iteritems():
+                    print k, q
+            print '-----------------------------------------'
+
+        for k, v in self.clusters.iteritems():
+            print k, v
 
     def get_abstraction(self):
         self.__get_community_detection()
         self.__get_count_groups()
         self.__get_abstraction_asterisk()
+        self.__print()
 
 
-aa = AutoAbstraction('/home/hudan/Git/datasets/casper-rw/logs/auth.log')
+aa = AutoAbstraction('/home/hudan/Git/datasets/casper-rw/logs/dmesg')
 aa.get_abstraction()
